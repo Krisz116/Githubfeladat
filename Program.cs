444bde581556a95c9f81413528724eea4f43832b
Krisz116 <30365653+Krisz116@users.noreply.github.com>
2022-09-21 12:48:46 +0200

    3.0

--- Program.cs
+++ Program.cs
@@ -15,6 +15,16 @@ namespace githubfeladat0
             int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Adja meg a b értékét");
             int b = Convert.ToInt32(Console.ReadLine());
+
+
+            if (a> b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
         }
     }
 }
